8c54f28f1e80cd5997e427cf4a521aa319a435f9
Pedro-2201 <pedrohmxavier@hotmail.com>
2024-06-20 05:19:51 -0300

    feat: implementacao da conta bancaria via terminal

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = leitor.nextLine();
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = leitor.nextInt();
+        leitor.nextLine();
+        System.out.print("Digite o número da agência: ");
+        String numeroAgencia = leitor.nextLine();
+        System.out.print("Digite o saldo da conta: ");
+        double saldoConta = leitor.nextDouble();
+        leitor.nextLine();
+        System.out.print(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, numeroAgencia, numeroConta, saldoConta));
+    }
+}
