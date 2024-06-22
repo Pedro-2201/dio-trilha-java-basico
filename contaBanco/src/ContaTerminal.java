66aeda42928a8fdb37199b0e8919c153e15b18f3
Pedro-2201 <pedrohmxavier@hotmail.com>
2024-06-21 22:04:36 -0300

    chore: add import locale

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -11,9 +12,8 @@ public class ContaTerminal {
         leitor.nextLine();
         System.out.print("Digite o número da agência: ");
         String numeroAgencia = leitor.nextLine();
-        System.out.print("Digite o saldo da conta: ");
-        double saldoConta = leitor.nextDouble();
-        leitor.nextLine();
-        System.out.print(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, numeroAgencia, numeroConta, saldoConta));
+        System.out.print("Digite o saldo da conta: "); //Usar a vírgula para separar os decimais
+        float saldoConta = leitor.nextFloat();
+        System.out.print(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, numeroAgencia, numeroConta, saldoConta));
     }
 }
